3cf9f719ba6830c89566e5f92ff1c640b1c26352
Nicolas Widart <n.widart@gmail.com>
2013-03-22 21:42:21 +0100

    Mobile aditions.

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="user-scalable=no, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
 	<meta content="yes" name="apple-mobile-web-app-capable"/>
 	<meta name="apple-mobile-web-app-status-bar-style" content="black"/>
+	<meta name="apple-mobile-web-app-title" content="Ikki">
 
 	<title>iLab - IkkI</title>
 	<style>
@@ -12,7 +13,9 @@
 	</style>
 	<link href="assets/css/bootstrap.css" rel="stylesheet">
 	<link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+	<link href="assets/css/add2home.css" rel="stylesheet">
 	<link href="assets/css/style.css" rel="stylesheet">
+	<script src="assets/js/add2home.js"></script>
 </head>
 
 <body>
@@ -39,14 +42,13 @@
 
 <div class="container-fluid">
 	<div class="row">
-		<div class="span5" >
-			<div class="well">
+			<div class="alert alert-block">
+				<button type="button" class="close" data-dismiss="alert">&times;</button>
 				<h4>Intro</h4>
-
+				<p class="text-error">It's better to view this Web app after placing it on the homescreen.</p>
 				<p>This a prototype. It uses the wikilocation API & wikipedia API to get the close buildings & places near your current location.</p>
 				<p><a href="https://github.com/CubesNCircles/iLab-Ikki" target="_blank">View source</a> | <a href="https://cubesncircles.wordpress.com">View project blog</a></p>
 			</div>
-		</div>
 		<div class="span5" id="right"></div>
 	</div>
 </div>
